191f15aea59ff4869af3a4229b8e556c525f3f8d
Sanjeev Sariya <sariya.sanjeev@gatech.edu>
2016-03-14 09:46:33 -0400

    added comments in main c

--- rdp_otu_parse_c/main.c
+++ rdp_otu_parse_c/main.c
@@ -23,10 +23,10 @@ int main(int argc, char * argv[]){
     print_help();
   }
   char get_opt;
-  const char* rdp_file=NULL;
-  const char* map_file=NULL;
-  const char* run_name=NULL; 
-  float confid=0.8;
+  const char* rdp_file=NULL; /*rdp file*/
+  const char* map_file=NULL; /*otu-map file*/
+  const char* run_name=NULL; /*output prefix file name*/
+  float confid=0.8; /*conf while parsing RDP output*/
 
   while((get_opt=getopt(argc, argv,"n:r:c:m:")) !=-1){
     
@@ -35,7 +35,7 @@ int main(int argc, char * argv[]){
       rdp_file=optarg;
       break;
     case 'c':
-      confid=atof(optarg);
+      confid=atof(optarg);/*covert to float*/
       break;
     case 'm':
       map_file=optarg;
@@ -45,14 +45,17 @@ int main(int argc, char * argv[]){
       break;
     case '?':
       print_help();
+      /*anything else - break and print help*/
       break;
     }
     /*switch case ends*/
   }/*while ends*/
+  
   printf("RDP %s\n",rdp_file);
   printf ("Map file %s\n",map_file);
   printf("RUN name %s\n",run_name);
   printf("Confidence used is %f\n",confid);
+  
   parse_otu_map_file(map_file);
   return 0;
 }
