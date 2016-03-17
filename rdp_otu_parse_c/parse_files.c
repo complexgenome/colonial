aa41fed5d69885dbb6b025c208b0e4e025d4ad9a
Sanjeev Sariya <sariya.sanjeev@gatech.edu>
2016-03-17 13:30:54 -0400

    free 2D array, store split-line

--- rdp_otu_parse_c/parse_files.c
+++ rdp_otu_parse_c/parse_files.c
@@ -11,8 +11,9 @@ int parse_otu_map_file(const char *otu_file){
 
   char line[SIZE_BUFF]; /*store line*/  
   FILE *otu_handle=fopen(otu_file,"r");
-  char *test=NULL;
-  
+  char **split_array=NULL;/*store split line values*/
+  unsigned int val=0;
+
   if(otu_handle){
     
     Rep_seq *rep_seq_head;
@@ -27,11 +28,13 @@ int parse_otu_map_file(const char *otu_file){
     while(fgets(line, sizeof(line), otu_handle)){
       trim_character('\n',line);
       
-      printf("%s\n",line);
-      /*printf("Rep seq's value: %s %p\n",rep_seq_head->seq,rep_seq_head->next);*/
-      /*printf("tab are %u\n",get_tab_count(line));*/
+      printf("Rep seq's value:%p %s %p\n",rep_seq_head,rep_seq_head->seq,rep_seq_head->next);
       
-      array_of_str(line,get_tab_count(line)+1);
+      val=get_tab_count(line); /*get number of words/tab*/
+      split_array=array_of_str(line,val+1);
+      add_node(rep_seq_head,split_array,val);
+      
+      free_array(&split_array,val);/*Free memory of split line*/
     }
     /*looping file ends*/
     fclose(otu_handle);/*close file*/
@@ -52,38 +55,46 @@ void parse_rdp_file(const char *rdp_file){
   
 }
 /*Function ends to read RDP file ---------------*/
-void array_of_str(char *line, unsigned int count){
+void free_array(char ***array, unsigned int size){
+  
+  /* Free 2 D array of split strings
+   */
+  char **temp_add=*array;
+  
+  for(unsigned int i=0;i<size;i++){
+    /*Foor loop intial declarations in: C99, C11, or gnu99
+     */
+
+    free(temp_add[i]);
+  }
+  
+  free(temp_add);
+  *array=NULL;
+}
+/*Function ends-------------------------------------*/
+char **array_of_str(char *line, unsigned int count){
   
   unsigned int i=0;
-  /* char *array[count];*/
-  char *p;
-  char **h=malloc(count*sizeof(char *));
+  char *parsed_word;/*hold strtok output*/
+  char **array_words=malloc(count*sizeof(char *)); /*2D array */
   
-  p=strtok(line,"\t");
-  /*array[i]=strtok(line,"\t");*/
+  parsed_word=strtok(line,"\t");
 
-  while(p!=NULL){
-    /*  while(array[i] !=NULL){*/
-    h[i]=malloc(sizeof(p)*sizeof(char));
-    h[i++]=p;
-    /*h[i]=p;*/
-    /*i++;*/
-      
-    /*array[i]=strtok(NULL,"\t");*/
-    p=strtok(NULL,"\t");
-  }
-  
-  while(i--!=1){
-
-    printf("value is %s\n",h[i-1]);
+  while(parsed_word!=NULL && i < count ){
     
+    array_words[i]=malloc(strlen(parsed_word)+1);//'\0' last place
+    strcpy(array_words[i++],parsed_word); //copy chracters. we need them outside scope
+    parsed_word=strtok(NULL,"\t");
   }
-  printf("##--##\n");
-
-}
-/**/
-unsigned int get_tab_count(char *line){
+  //while ends
   
+  return array_words;
+}
+/*Function ends ----------------------------*/
+unsigned int get_tab_count(char *line){
+  /*
+   *return number of tabs present in line
+   */
   unsigned int count=0,i=0;
   
   while(*(line+i)!='\0'){
@@ -108,3 +119,11 @@ void trim_character(char chr,char *chr_array){
   *(chr_array+i)='\0';
 }
 /*Function ends---------------------------*/
+void add_node(Rep_seq *local,char **temp_array, unsigned int val){
+  
+  printf("We are here %p %d\n",local,val);
+  for(unsigned int i=0;i<val;i++){
+    printf("string is %s\n",temp_array[i]);
+  }
+}
+/*---------------------------*/
--- rdp_otu_parse_c/parse_files.h
+++ rdp_otu_parse_c/parse_files.h
@@ -12,5 +12,7 @@ int parse_otu_map_file(const char *); /*parse rep-otu map file*/
 void trim_character(char, char *);/*replace given character with '\0'*/
 unsigned int get_tab_count(char *); /*get tab count*/
 
-void array_of_str(char *, unsigned int);
+char **array_of_str(char *, unsigned int);/*split and store in array*/
+void free_array(char ***, unsigned int); /*Free two dim array*/
+
 #endif
