5d690bfab7edd8ca18862e468c628dc82823e535
Sanjeev Sariya <sariya.sanjeev@gatech.edu>
2016-03-17 18:25:29 -0400

    edited print rep-link-list

--- rdp_otu_parse_c/rep_seq.c
+++ rdp_otu_parse_c/rep_seq.c
@@ -10,15 +10,16 @@ void print_list(struct Rep_seq *temp){
   if(temp!=NULL){
     struct Sample_count *sc_temp;
     
-    while(temp->next){
+    while(temp->next!=NULL){
       printf("Rep seq we have is %s\n",temp->seq);
       sc_temp=temp->sample_count;
       printf("In rep seq. Sample name is %p \n",(void*)sc_temp);
-      //printf("Sample name is %s\n",sc_temp->name);
 	
       temp=temp->next;
     }
-
+    sc_temp=temp->sample_count;
+    printf("Rep seq we have is %p\n",(void *)sc_temp);
+    
   }
   /*If valid node*/
 }
