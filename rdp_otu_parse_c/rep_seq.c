5d294a0235c9b19027e891511b9024b3013b0567
Sanjeev Sariya <sariya.sanjeev@gatech.edu>
2016-03-18 14:50:08 -0400

    Code: change in sample_name struct

--- rdp_otu_parse_c/rep_seq.c
+++ rdp_otu_parse_c/rep_seq.c
@@ -3,23 +3,28 @@
 
 #include "rep_seq.h"
 
-void delete_rep_node(struct Rep_seq *temp){
-  
-}
+//void delete_rep_node(struct Rep_seq *temp){}
+
+
 void print_list(struct Rep_seq *temp){
   if(temp!=NULL){
-    struct Sample_count *sc_temp;
+    struct Sample_name *temp_sc;
     
     while(temp->next!=NULL){
-      printf("Rep seq we have is %s\n",temp->seq);
-      sc_temp=temp->sample_count;
-      printf("In rep seq. Sample name is %p \n",(void*)sc_temp);
-	
+      
+      temp_sc=temp->sample_name_head;
+      printf("Rep seq we have is %s\t",temp->seq);
+
+      while(temp_sc->next!=NULL){
+	printf("Sample is %s %u\t",temp_sc->name,temp_sc->count);
+	temp_sc=temp_sc->next;
+      }
+      /*loop over samples ----*/
+      printf("Final/First print is %s %u\n",temp_sc->name,temp_sc->count);
       temp=temp->next;
     }
-    sc_temp=temp->sample_count;
-    printf("Rep seq we have is %p\n",(void *)sc_temp);
-    
+    /*loop over rep sequences */
+    printf("*--Leaving print--*\n");
   }
   /*If valid node*/
 }
