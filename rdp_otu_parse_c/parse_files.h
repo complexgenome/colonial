3e3fff8728db6610fdd217465e73fe11a5f43366
Sanjeev Sariya <sariya.sanjeev@gatech.edu>
2016-03-21 14:02:30 -0400

    code: BUFF changed- OTU and RDP

--- rdp_otu_parse_c/parse_files.h
+++ rdp_otu_parse_c/parse_files.h
@@ -1,16 +1,16 @@
-
 #ifndef PARSE_FILE
 #define PARSE_FILE
 
 #define _GNU_SOURCE
 #include<string.h>
 
-#define SIZE_BUFF 8000
+#define OTU_SIZE_BUFF 8000
+#define RDP_SIZE_BUFF 4000
 
-void parse_rdp_file(const char *); /*parse rdp classictn file*/
+struct Microbiome *parse_rdp_file(const char *); /*parse rdp classictn file*/
 
-//int parse_otu_map_file(const char *); /*parse rep-otu map file*/
-struct Rep_seq * parse_otu_map_file(const char *); /*parse rep-otu map file*/
+struct Rep_seq *parse_otu_map_file(const char *); /*parse rep-otu map file*/
+
 
 void trim_character(char, char *);/*replace given character with '\0'*/
 unsigned int get_tab_count(char *); /*get tab count*/
